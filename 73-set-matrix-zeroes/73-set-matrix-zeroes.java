f91636cc8684526e1af227aae12d40747af0c8d8
salonipriyani <salonipriyani98@gmail.com>
2022-04-15 13:34:20 +0530

    Time: 2 ms (41.19%), Space: 44.1 MB (81.81%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -2,37 +2,26 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean[][] isZero = new boolean[m][n];
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
         int i, j;
         
         for (i = 0; i < m; i++){
-            for (j = 0;  j < n; j++){
-                if(matrix[i][j] == 0)
-                    isZero[i][j] = true;
-            }
-        }
-        
-        for (i = 0; i< m; i++){
             for (j = 0; j < n; j++){
-                if (isZero[i][j] == true){
-                    makeRowZero(matrix, i, n);
-                    makeColumnZero(matrix, j, m);
+                if (matrix[i][j] == 0){
+                    rows.add(i);
+                    cols.add(j);
                 }
             }
         }
         
-        
+        for (i = 0; i < m; i++){
+            for (j = 0; j < n; j++){
+                if (rows.contains(i) || cols.contains (j)){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
     
-    public void makeRowZero(int[][] matrix, int i, int n){
-        for(int k = 0; k < n; k++){
-            matrix[i][k] = 0;
-        }
-    }
-
-    public void makeColumnZero(int[][] matrix, int j, int m){
-        for(int k = 0; k < m; k++){
-            matrix[k][j] = 0;
-        }
-    }
 }
