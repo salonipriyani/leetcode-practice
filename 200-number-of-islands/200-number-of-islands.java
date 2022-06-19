dd9efd5111ee2dddd7c3c8ebe13b0ac5263bf637
salonipriyani <salonipriyani98@gmail.com>
2022-06-19 16:17:07 +0530

    Time: 13 ms (14.64%), Space: 55.5 MB (76.65%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,25 +1,35 @@
+//BFS
 class Solution {
     public int numIslands(char[][] grid) {
         int rows = grid.length, cols = grid[0].length, count = 0;
+        boolean[][] visited = new boolean[rows][cols];
+        Queue<int[]> q = new LinkedList<>();
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < cols; j++){
-                if (grid[i][j] == '1'){
-                    dfs(grid, i, j);
+                if (grid[i][j] == '1' && !visited[i][j]){
+                    q.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                    bfs(grid, visited, q);
                     count++;
                 }
             }
         }
         return count;
     }
-    
-    public void dfs(char[][] grid, int i, int j){
+    int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    public void bfs(char[][] grid, boolean[][] visited, Queue<int[]> q){
         int rows = grid.length, cols = grid[0].length;
-        if (i >= 0 && j >= 0 && i < rows && j < cols && grid[i][j] == '1'){
-            grid[i][j] = '0';
-            dfs(grid, i + 1, j);
-            dfs(grid, i - 1, j);
-            dfs(grid, i, j + 1);
-            dfs(grid, i, j - 1);
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            for (int[] dir : dirs){
+                int x = cur[0] + dir[0];
+                int y = cur[1] + dir[1];
+                if (x < 0 || y < 0 || x >= rows || y >= cols || visited[x][y] || grid[x][y] == '0')
+                    continue;
+                
+                visited[x][y] = true;
+                q.offer(new int[]{x, y});
+            }
         }
     }
 }
