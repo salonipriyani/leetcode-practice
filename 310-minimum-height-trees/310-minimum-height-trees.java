5b1765d82e11fee077aa6e26143f8d05fbe693c3
salonipriyani <salonipriyani98@gmail.com>
2022-06-28 23:20:09 +0530

    Time: 84 ms (20.58%), Space: 87.6 MB (10.55%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.java
+++ 310-minimum-height-trees/310-minimum-height-trees.java
@@ -1,62 +1,62 @@
-// class Solution {
-//     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-//         if (n == 1)
-//             return Arrays.asList(0);
-//         Map<Integer, Set<Integer>> map = new HashMap<>();
-//         for (int i = 0; i < n; i++)
-//             map.put(i, new HashSet<>());
-        
-//         for (int[] edge : edges){
-//             map.get(edge[0]).add(edge[1]);
-//             map.get(edge[1]).add(edge[0]);
-//         }
-        
-//         List<Integer> leaves = new ArrayList<>();
-//         for (int i = 0; i < n; i++){
-//             if(map.get(i).size() == 1)
-//                 leaves.add(i);
-//         }
-        
-//         while (n > 2){
-//             n =- leaves.size();
-//             List<Integer> newLeaves = new ArrayList<>();
-//             for (int i : leaves){
-//                 int j = map.get(i).iterator().next();
-//                 map.get(j).remove(i);
-//                 if (map.get(j).size() == 1)
-//                     newLeaves.add(j);
-//             }
-//             leaves = newLeaves;
-//         }
-        
-//         return leaves;
-//     }
-// }
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if (n == 1) return Collections.singletonList(0);
-
-        List<Set<Integer>> adj = new ArrayList<>(n);
-        for (int i = 0; i < n; ++i) adj.add(new HashSet<>());
-        for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+        if (n == 1)
+            return Arrays.asList(0);
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            map.put(i, new HashSet<>());
+        
+        for (int[] edge : edges){
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
         }
-
+        
         List<Integer> leaves = new ArrayList<>();
-        for (int i = 0; i < n; ++i)
-            if (adj.get(i).size() == 1) leaves.add(i);
-
-        while (n > 2) {
+        for (int i = 0; i < n; i++){
+            if(map.get(i).size() == 1)
+                leaves.add(i);
+        }
+        
+        while (n > 2){
             n -= leaves.size();
             List<Integer> newLeaves = new ArrayList<>();
-            for (int i : leaves) {
-                int j = adj.get(i).iterator().next();
-                adj.get(j).remove(i);
-                if (adj.get(j).size() == 1) newLeaves.add(j);
+            for (int i : leaves){
+                int j = map.get(i).iterator().next();
+                map.get(j).remove(i);
+                if (map.get(j).size() == 1)
+                    newLeaves.add(j);
             }
             leaves = newLeaves;
         }
+        
         return leaves;
     }
 }
+// class Solution {
+//     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+//         if (n == 1) return Collections.singletonList(0);
+
+//         List<Set<Integer>> adj = new ArrayList<>(n);
+//         for (int i = 0; i < n; ++i) adj.add(new HashSet<>());
+//         for (int[] edge : edges) {
+//             adj.get(edge[0]).add(edge[1]);
+//             adj.get(edge[1]).add(edge[0]);
+//         }
+
+//         List<Integer> leaves = new ArrayList<>();
+//         for (int i = 0; i < n; ++i)
+//             if (adj.get(i).size() == 1) leaves.add(i);
+
+//         while (n > 2) {
+//             n -= leaves.size();
+//             List<Integer> newLeaves = new ArrayList<>();
+//             for (int i : leaves) {
+//                 int j = adj.get(i).iterator().next();
+//                 adj.get(j).remove(i);
+//                 if (adj.get(j).size() == 1) newLeaves.add(j);
+//             }
+//             leaves = newLeaves;
+//         }
+//         return leaves;
+//     }
+// }
