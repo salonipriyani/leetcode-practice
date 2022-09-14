a0d7da8404766f63f2cb9309b265afa2c01ffe19
salonipriyani <salonipriyani98@gmail.com>
2022-09-14 02:02:07 -0400

    Time: 136 ms (15.56%), Space: 113.7 MB (21.34%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -1,29 +1,18 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int sum = 0, left = 0, temp;
-        Map<Integer, Integer> map = new HashMap<>();
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < fruits.length; i++){
-            map.put(fruits[i], map.getOrDefault(fruits[i], 0) + 1);
-            sum++;
-            
-            if(map.size() > 2){
-                while(map.size() > 2){
-                    if(map.containsKey(fruits[left])){
-                        if(map.get(fruits[left]) == 1)
-                            map.remove(fruits[left]);
-                        else
-                            map.put(fruits[left], map.get(fruits[left]) - 1);
-                    }
-                    left++;
-                    sum--;
-                }
+        Map<Integer, Integer> fruitFreq = new HashMap<>();
+        int windowStart = 0, maxLen = Integer.MIN_VALUE;
+        for (int windowEnd = 0; windowEnd < fruits.length; windowEnd++){
+            fruitFreq.put(fruits[windowEnd], fruitFreq.getOrDefault(fruits[windowEnd], 0) + 1);
+            while (fruitFreq.size() > 2){
+                fruitFreq.put(fruits[windowStart], fruitFreq.get(fruits[windowStart]) - 1);
+                if (fruitFreq.get(fruits[windowStart]) == 0)
+                    fruitFreq.remove(fruits[windowStart]);
+                windowStart++;
             }
-            else {
-                max = Math.max(max, sum);
-            }
+            maxLen = Math.max(maxLen, windowEnd - windowStart + 1);
             
         }
-        return max;
+        return maxLen;
     }
 }
