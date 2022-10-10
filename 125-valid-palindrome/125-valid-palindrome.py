d6497fc4de61d621df9a28e6e6214ec398b93ef9
salonipriyani <salonipriyani98@gmail.com>
2022-10-10 00:41:48 -0400

    Time: 56 ms (85.51%), Space: 14.4 MB (85.51%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -5,14 +5,14 @@ class Solution:
         
         while i < j:
             while i < j and not s[i].isalnum():
-                i += 1
+                i = i + 1
             while i < j and not s[j].isalnum():
-                j -= 1
-                
+                j = j - 1
+            
             if s[i].lower() != s[j].lower():
                 return False
             
-            i += 1
-            j -= 1
-        
+            i = i + 1
+            j = j - 1
+            
         return True
