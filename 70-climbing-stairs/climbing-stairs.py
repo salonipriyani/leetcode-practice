b5642dd44a140042bdf242e52989b44e010ef690
salonipriyani <salonipriyani98@gmail.com>
2024-12-20 23:22:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (6.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -13,11 +13,13 @@ class Solution:
         return dfs(0)
 
 #Top down - recursion but with cache
+# Time - O(n) and space - O(n)
 class Solution:
     def climbStairs(self, n: int) -> int:
         cache = [-1] * n
         
         def dfs(i):
+            print(cache)
             if i > n:
                 return 0
             elif i == n:
@@ -29,3 +31,15 @@ class Solution:
         
         return dfs(0)
         
+#Bottom Up
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+        return dp[n]
