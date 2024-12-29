5efd065d41d07c6e3cdda804e635e3b0e1dd276d
salonipriyani <salonipriyani98@gmail.com>
2024-12-29 01:36:19 -0500

    Time: 765 ms (61.42%) | Memory: 18.1 MB (39.50%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,15 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {0 : 0}
-        def dfs(amount):
-            if amount in dp:
-                return dp[amount]
-            
-            res = 1e9
-            for coin in coins:
-                if amount - coin >= 0:
-                    res = min(res, 1 + dfs(amount - coin))
-            dp[amount] = res
-            return res
-        res = dfs(amount)
-        return -1 if res >= 1e9 else res
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a-c])
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
+        
