3bed093f592a91057ec741ffd5d3749b516c2490
salonipriyani <salonipriyani98@gmail.com>
2025-01-08 20:06:35 -0500

    Time: 639 ms (54.25%) | Memory: 20 MB (51.16%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,21 +4,17 @@ class Solution:
             return False
         target = sum(nums) // 2
         n = len(nums)
-        dp = [[-1] * (target + 1) for _ in range(len(nums))]
+        dp = set()
+        dp.add(0)
 
-        def dfs(i, target):
-            if target == 0:
-                return True
-            if i >= n or target < 0:
-                return False
-            
-            if dp[i][target] != -1:
-                return dp[i][target]
-            
-            dp[i][target] = dfs(i + 1, target) or dfs(i + 1, target - nums[i])
-            return dp[i][target]
+        for i in range(n - 1, -1, -1):
+            nextdp = set()
+            for t in dp:
+                nextdp.add(t)
+                nextdp.add(t + nums[i])
+            dp = nextdp
         
-        return dfs(0, sum(nums) // 2)
+        return True if target in dp else False
 
 
 
