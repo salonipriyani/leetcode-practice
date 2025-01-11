9b9018aa5c93b5cd884fd8c4b90460249ea34148
salonipriyani <salonipriyani98@gmail.com>
2025-01-11 16:21:16 -0500

    Time: 69 ms (44.89%) | Memory: 21.1 MB (33.74%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,21 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = {}
+        
         m = len(word1)
         n = len(word2)
-        def dfs(i, j):
-            if i == len(word1):
-                return n - j
-            if j == len(word2):
-                return m - i
 
-            if (i, j) in dp:
-                return dp[(i, j)]
-
-            if word1[i] == word2[j]:
-                dp[(i, j)] = dfs(i + 1, j + 1)
-            else:
-                res = min(dfs(i + 1, j), dfs(i, j + 1), dfs(i + 1, j + 1))
-                dp[(i, j)] = res + 1
-            return dp[(i, j)]
-        return dfs(0, 0)
+        dp = [[float('-inf')] * (n + 1) for i in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][n] = m - i
+        for j in range(n + 1):
+            dp[m][j] = n - j
+        
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+        return dp[0][0]
+        
