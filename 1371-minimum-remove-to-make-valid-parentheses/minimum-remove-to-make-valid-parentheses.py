d301771ed036607b1fd1154cf7c028f1186f4e83
salonipriyani <salonipriyani98@gmail.com>
2025-02-05 22:51:47 -0500

    Time: 43 ms (60.01%) | Memory: 19.3 MB (53.85%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -3,16 +3,15 @@ class Solution:
         stack = []
         newstr = ""
         for i, letter in enumerate(s):
-            if len(stack) == 0 and (letter == '(' or letter == ')'):
-                stack.append([i, letter])
+            if letter == '(' or letter == ')':
+                if len(stack) == 0:
+                    stack.append([i, letter])
 
-            elif letter == ')':
-                if stack[-1][1] == '(':
+                elif letter == ')' and stack[-1][1] == '(':
                     stack.pop()
                 else:
                     stack.append([i, letter])
-            elif letter == '(':
-                stack.append([i, letter])
+                
         print(stack)
         s_list = list(s)
         for i, letter in stack: 
