863b45cb72543ceef9b5980d82561a484328ecee
salonipriyani <salonipriyani98@gmail.com>
2025-02-07 14:57:05 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.83%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -4,21 +4,20 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        nums1_cp = nums1[:m]
+        p1, p2, p = m - 1, n - 1, m + n - 1
+        
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
 
-        p1 = 0
-        p2 = 0
-
-        while p1 < m and p2 < n:
-            if nums1_cp[p1] < nums2[p2]:
-                nums1[p1 + p2] = nums1_cp[p1]
-                p1 += 1
-            elif nums1_cp[p1] >= nums2[p2]:
-                nums1[p1+p2] = nums2[p2]
-                p2 += 1
-            
-        if p1 < m:
-            nums1[p1 + p2 : m + n] = nums1_cp[p1:]
-        if p2 < n:
-            nums1[p1 + p2 : m + n] = nums2[p2:]
+        # If nums2 is not exhausted, copy the remaining elements
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
         
