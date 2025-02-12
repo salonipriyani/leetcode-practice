a4a587892bc00108c35c3aca86f848ae34470fa0
salonipriyani <salonipriyani98@gmail.com>
2025-02-11 23:27:10 -0500

    Time: 53 ms (6.31%) | Memory: 18.7 MB (9.77%) - LeetSync

---
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        if not head:
+            node = Node(insertVal)
+            node.next = node
+            return node
+        
+        prev = head
+        curr = head.next
+
+        while True:
+            if prev.val <= insertVal <= curr.val:
+                prev.next = Node(insertVal, curr)
+                return head
+            elif prev.val > curr.val:
+                if insertVal < curr.val or insertVal > prev.val:
+                    prev.next = Node(insertVal, curr)
+                    return head
+        
+            prev = curr
+            curr = curr.next
+            if prev == head:
+                break
+        
+        prev.next = Node(insertVal, curr)
+        return head
+        
+            
