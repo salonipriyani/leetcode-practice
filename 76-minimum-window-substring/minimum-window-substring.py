57a1e023a71ca9eecefb2e0312a338d53dfc2528
salonipriyani <salonipriyani98@gmail.com>
2025-03-13 20:40:13 -0400

    Time: 70 ms (69.07%) | Memory: 18.4 MB (17.33%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,44 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         t_freq_map = {}
-        
 
         for char in t:
             if char not in t_freq_map:
                 t_freq_map[char] = 0
             t_freq_map[char] += 1
-        
-        required = len(t_freq_map)
         formed = 0
+        required = len(t_freq_map)
+        l = 0
         min_window = float('inf')
         min_l = 0
         min_r = 0
-        l = 0
-        window_counts = {}
-        for r, char in enumerate(s):
-            print(char)
-            print(s[l])
-            if char not in window_counts:
-                window_counts[char] = 0
-            window_counts[char] += 1
+        window_freq = {}
+        for r in range(len(s)):
             
-            if char in t_freq_map and t_freq_map[char] == window_counts[char]:
+            if s[r] not in window_freq:
+                window_freq[s[r]] = 0
+            window_freq[s[r]] += 1
+            if s[r] in t_freq_map and t_freq_map[s[r]] == window_freq[s[r]]:
                 formed += 1
-            
-            while l <= r and formed == required:
-
+            while l <= r and required == formed:
                 window_size = r - l + 1
                 if window_size < min_window:
                     min_window = window_size
                     min_l = l
                     min_r = r
 
-                window_counts[s[l]] -= 1
-                if s[l] in t_freq_map and window_counts[s[l]] < t_freq_map[s[l]]:
+                window_freq[s[l]] -= 1
+                if s[l] in t_freq_map and window_freq[s[l]] < t_freq_map[s[l]]:
                     formed -= 1
                 l += 1
-            
-
-            
             
         return "" if min_window == float('inf') else s[min_l : min_r + 1]
