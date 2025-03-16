839688ac012de82809e9147640fc815cf4971c9f
salonipriyani <salonipriyani98@gmail.com>
2025-03-16 12:58:29 -0400

    Time: 250 ms (14.37%) | Memory: 20.2 MB (58.30%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,31 +1,37 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         leaves = []
-        dirs = [[0,1], [1, 0], [-1, 0], [0, -1]]
         outdegrees = {}
+        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         for r in range(len(matrix)):
             for c in range(len(matrix[0])):
-                outdegrees[(r,c)] = 0
-                for dr, dc in dirs:
-                    if r + dr >= 0 and r + dr < len(matrix) and c + dc >= 0 and c + dc < len(matrix[0]) and matrix[r + dr][c + dc] > matrix[r][c]:
-                        outdegrees[(r,c)] += 1
+                outdegrees[(r, c)] = 0
+                for dir in dirs:
+                    new_r = r + dir[0]
+                    new_c = c + dir[1]
+                    if new_r >= 0 and new_r < len(matrix) and new_c >= 0 and new_c < len(matrix[0]) and matrix[new_r][new_c] > matrix[r][c]:
+                        outdegrees[(r, c)] += 1
         
-        for rc, out in outdegrees.items():
-            r, c = rc
-            if outdegrees[rc] == 0:
-                leaves.append([r, c])
+
+        leaves = []
+        for cell in outdegrees:
+            if outdegrees[cell] == 0:
+                leaves.append(cell)
+
         height = 0
         while leaves:
             height += 1
-            newLeaves = []
+            newleaves = []
             for i in range(len(leaves)):
                 r, c = leaves[i]
                 for dr, dc in dirs:
-                    if r + dr >= 0 and r + dr < len(matrix) and c + dc >= 0 and c + dc < len(matrix[0]) and matrix[r + dr][c + dc] < matrix[r][c]:
-                        outdegrees[(r + dr, c + dc)] -= 1
-                        if outdegrees[(r + dr, c + dc)] == 0:
-                            newLeaves.append([r + dr, c + dc])
+                    new_r = r + dr
+                    new_c = c + dc
+                    if new_r >= 0 and new_r < len(matrix) and new_c >= 0 and new_c < len(matrix[0]) and matrix[new_r][new_c] < matrix[r][c]:
+                        outdegrees[(new_r, new_c)] -= 1
+                        if outdegrees[(new_r, new_c)] == 0:
+                            newleaves.append([r + dr, c + dc])
             
-            leaves = newLeaves
+            leaves = newleaves
+        
         return height
-         
