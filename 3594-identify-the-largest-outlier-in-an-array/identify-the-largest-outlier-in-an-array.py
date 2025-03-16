51fee312a70ee11cd422bc188303b8d002920743
salonipriyani <salonipriyani98@gmail.com>
2025-03-16 10:39:37 -0400

    Time: 300 ms (40.52%) | Memory: 28.1 MB (94.92%) - LeetSync

---
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        
+
+        total = sum(nums)
+        numset = set(nums)
+        num_map = {}
+
+        for num in nums:
+            if num not in num_map:
+                num_map[num] = 0
+            num_map[num] += 1
+
+        largest = float('-inf')
+        for num in nums:
+            potential_outlier = total - 2 * num
+
+            if potential_outlier in numset and (potential_outlier != num or num_map[num] > 1):
+                largest = max(largest, potential_outlier)
+
+        return largest
