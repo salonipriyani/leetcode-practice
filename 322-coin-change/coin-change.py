6ad2865d77eeee226b32280e37d57a5950da994d
salonipriyani <salonipriyani98@gmail.com>
2025-03-23 23:44:08 -0400

    Time: 1166 ms (16.04%) | Memory: 46 MB (6.55%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,12 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
+        map = {}
+        def recurse(amount):
+            if amount == 0:
+                return 0
+            if amount in map:
+                return map[amount]
+            res = float('inf')
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min(res, 1 + recurse(amount - coin))
+            map[amount] = res
+            return res
+            
 
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a-c])
-
-        return dp[amount] if dp[amount] != amount + 1 else -1
-        
+        res = recurse(amount)
+        return -1 if res == float('inf') else res
