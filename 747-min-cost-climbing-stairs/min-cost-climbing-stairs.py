bf14bc3c8b884a13265641ad6e72a7f58de70667
salonipriyani <salonipriyani98@gmail.com>
2025-10-27 23:02:29 -0400

    Time: 3 ms (73.54%) | Memory: 17.9 MB (63.36%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,12 +1,9 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cache = [-1] * len(cost)
-        def dfs(i):
-            if i >= len(cost):
-                return 0
-            if cache[i] != -1:
-                return cache[i]
-            cache[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
-            return cache[i]
+        n = len(cost)
+        dp = [0] * (n + 1)
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
         
-        return min(dfs(0), dfs(1))
+        return dp[n]
