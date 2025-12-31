9dc748f2bf65c52a6fc376a658f573a61791bfe6
salonipriyani <salonipriyani98@gmail.com>
2025-12-30 20:14:08 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (97.45%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,40 +1,14 @@
-#Recursion: Time = O(2 ^ n), space = O(n)
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        def dfs(i):
-            if i >= len(nums):
-                return 0
-            return max(nums[i] + dfs(i + 2), dfs(i + 1))
-        
-        return dfs(0)
-
-#Recursion - top-down: Time = O(n), space = O(n)
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        memo = [-1] * len(nums)
-
-        def dfs(i):
-            if i >= len(nums):
-                return 0
-            if memo[i] != -1:
-                return memo[i]
-            memo[i] = max(nums[i] + dfs(i + 2), dfs(i + 1))
-            return memo[i]
-        dfs(0)
-        return memo[0]
-#Recursion - bottom-up: Time = O(n), space = O(n)
-class Solution:
-    def rob(self, nums: List[int]) -> int:
+        ls = [0] * len(nums)
         if len(nums) == 1:
             return nums[0]
-        n = len(nums)
-        dp = [0] * (len(nums))
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        ls[0], ls[1] = nums[0], max(nums[0], nums[1])
 
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            ls[i] = max(nums[i] + ls[i - 2], ls[i - 1])
 
-        for i in range(2, n):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
-        
-        return dp[n - 1]
+        return ls[len(nums) - 1]
+
