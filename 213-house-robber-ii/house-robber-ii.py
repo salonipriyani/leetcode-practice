cec563a9482d84ed99b92944fe2689b3a8e97b7f
salonipriyani <salonipriyani98@gmail.com>
2025-12-30 20:57:41 -0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (89.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -7,14 +7,13 @@ class Solution:
         return max(self.helper(nums[1:]), self.helper(nums[:-1]))
     
     def helper(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        ls = [-1] * len(nums)
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if ls[i] != -1:
+                return ls[i]
+            ls[i] = max(nums[i] + dfs(i + 2), dfs(i + 1))
+            return ls[i]
         
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
-        for i in range(2, len(nums)):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
-        
-        return dp[-1]
+        return dfs(0)
