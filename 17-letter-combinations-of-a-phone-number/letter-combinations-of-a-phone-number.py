9b19d2ad9fc4ec51866798d8333275299a0f56d7
salonipriyani <salonipriyani98@gmail.com>
2026-01-27 22:20:15 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (30.77%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        digits_to_letters = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv", 
+            "9" : "wxyz" 
+        }
+
+        res = []
+
+        def dfs(i , curStr):
+            if i == len(digits):
+                res.append(curStr)
+                return
+
+            for char in digits_to_letters[digits[i]]:
+                dfs(i + 1, curStr + char)
+        
+        if digits:
+            dfs(0, "")
+        return res
